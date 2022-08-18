d8e2c1ca8fc48a890be88ea71a31d70a4f74f005
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-18 10:10:52 +0530

    Update plet.py

--- Shikimori/modules/plet.py
+++ Shikimori/modules/plet.py
@@ -70,7 +70,7 @@ __help__ = """
 `/plet` <text>: Creates a sticker with your Sending message
 """
 
-__mod_name__ = "Text To Sticker🌈"
+__mod_name__ = "Tᴇxᴛ Tᴏ Sᴛɪᴄᴋᴇʀ"
 __command_list__ = [
     "plet"
 ]
