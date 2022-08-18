56332c2b9f31b1520c4a675aa83580e9e67821b6
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-18 12:16:20 +0530

    Update waifu.py

--- Shikimori/modules/waifu.py
+++ Shikimori/modules/waifu.py
@@ -60,12 +60,12 @@ WAIFUINFO_HANDLER = CommandHandler('waifuinfo', waifu, run_async=True)
 dispatcher.add_handler(WAIFUINFO_HANDLER)
 
 #  Buttons for /help .
-__mod_name__ = 'Waifus'  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
+__mod_name__ = 'Wᴀɪғᴜs'  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
 
 
 # *IF YOU WANT* EDIT MESSAGE FOR HELP OF THIS MODULE.
 __help__ = '''
-*Get waifu images*
+*Gᴇᴛ Wᴀɪғᴜ Iᴍᴀɢᴇɢ*
 
    ➢ `/waifu`*:* Sends limited but best Waifu image. *RECOMMENDED*
    ➢ `/waifuinfo`*:* Gives random image of waifu with info. 
