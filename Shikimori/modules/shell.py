33912e6a9b19b84a022a7e209f5dc1d66209f0a8
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-18 10:22:24 +0530

    Update shell.py

--- Shikimori/modules/shell.py
+++ Shikimori/modules/shell.py
@@ -68,6 +68,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh", "shell"], shell, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "Shell"
+__mod_name__ = "Sʜᴇʟʟ"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
