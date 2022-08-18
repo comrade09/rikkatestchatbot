dbe22d17d8079f72dc5c69e9806bf30ccf78d18b
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-18 20:33:32 +0530

    Update start.py

--- Shikimori/modules/start.py
+++ Shikimori/modules/start.py
@@ -141,7 +141,7 @@ def start(update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         first_name = update.effective_user.first_name
         chat_name = dispatcher.bot.getChat(chat_id).title
-        start_text= "*Hey {}, I'm here for you at {} since :* `{}`\n".format(escape_markdown(first_name), escape_markdown(chat_name), uptime)
+        start_text= "*Hᴇʏ {}, I'ᴍ Kᴀᴏʀɪ Mɪʏᴀᴢᴏɴᴏ I'ᴍ Hᴇʀᴇ Fᴏʀ Yᴏᴜ Aᴛ {} Sɪɴᴄᴇ :* `{}`\n".format(escape_markdown(first_name), escape_markdown(chat_name), uptime)
         try:
             if start_id in ("jpeg", "jpg", "png"):
                 update.effective_message.reply_photo(
