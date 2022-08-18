03da24b5f95660a6e24a620c3ee4423e688873cc
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-18 09:20:18 +0530

    Update eval.py

--- Shikimori/modules/eval.py
+++ Shikimori/modules/eval.py
@@ -148,4 +148,4 @@ CLEAR_HANDLER = CommandHandler("clearlocals", clear, run_async=True)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "Eval Module"
+__mod_name__ = "Eᴠᴀʟ Mᴏᴅᴜʟᴇ"
