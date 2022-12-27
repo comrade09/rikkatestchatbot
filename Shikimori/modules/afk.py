91af2615b195b045468a46439ca2260200b18f0c
comrade09 <106185020+comrade09@users.noreply.github.com>
2022-12-27 23:40:15 +0530

    Update afk.py

--- Shikimori/modules/afk.py
+++ Shikimori/modules/afk.py
@@ -25,6 +25,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import time
+from .redis import REDIS
 
 from telegram import MessageEntity, ParseMode
 from telegram.error import BadRequest
@@ -33,7 +34,6 @@ from telegram.ext import Filters, MessageHandler
 from Shikimori import dispatcher
 from Shikimori.modules.disable import DisableAbleCommandHandler
 from Shikimori.modules.redis.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
-from Shikimori import REDIS
 from Shikimori.modules.users import get_user_id
 
 from Shikimori.modules.helper_funcs.readable_time import get_readable_time
@@ -47,8 +47,6 @@ def afk(update, context):
     if not user:  # ignore channels
         return
 
-    if user.id == 777000:
-        return
     start_afk_time = time.time()
     if len(args) >= 2:
         reason = args[1]
@@ -58,9 +56,15 @@ def afk(update, context):
     REDIS.set(f'afk_time_{update.effective_user.id}', start_afk_time)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_animation("https://telegra.ph/file/a33ccd9bbf20ebae92f3f.mp4", caption="{} is now Away!".format(fname))
+        Shikimori = update.effective_message.reply_text(
+            "*{}* is now AFK! GoodBye!".format(fname), parse_mode=ParseMode.MARKDOWN)
+        time.sleep(5)
+        try:
+            Shikimori.delete()
+        except BadRequest:
+            pass
     except BadRequest:
-        pass
+         pass
 
 def no_longer_afk(update, context):
     user = update.effective_user
@@ -78,8 +82,13 @@ def no_longer_afk(update, context):
             return
         firstname = update.effective_user.first_name
         try:
-            message.reply_text(
-                "{} is no longer AFK!\nTime you were AFK for: {}".format(firstname, end_afk_time))
+             Shikimori = message.reply_text(
+                "*{}* is back in the chat!\nCame back after: `{}`".format(firstname, end_afk_time), parse_mode=ParseMode.MARKDOWN)
+             time.sleep(5)
+             try:
+                 Shikimori.delete()
+             except BadRequest:
+                 pass
         except Exception:
             return
 
@@ -140,34 +149,36 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if reason == "none":
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is AFK!\nSince: {}".format(fst_name, since_afk)
-            update.effective_message.reply_text(res)
+            res = "*{}* is busy right now!\nSince: `{}`".format(fst_name, since_afk)
+            update.effective_message.reply_text(res, parse_mode=ParseMode.MARKDOWN)
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is AFK! Says it's because of:\n{}\nSince: {}".format(fst_name, reason, since_afk)
-            update.effective_message.reply_text(res)
-
+            res = "*{}* is busy right now!\n*Reason*: `{}`\n*Away Time*: `{}`".format(fst_name, reason, since_afk)
+            Shikimori = update.effective_message.reply_text(res, parse_mode=ParseMode.MARKDOWN)
+            time.sleep(5)
+            try:
+                Shikimori.delete()
+            except BadRequest:
+                pass
 
 def __user_info__(user_id):
     is_afk = is_user_afk(user_id)
     text = ""
     if is_afk:
         since_afk = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user_id}'))))
-        text = "<i>This user is currently afk (away from keyboard).</i>"
-        text += f"\n<i>Since: {since_afk}</i>"
+        text = "This user is currently afk (away from keyboard)."
+        text += f"\nLast Seen: {since_afk} Ago."
        
     else:
-        text = "<i>This user is currently isn't afk (away from keyboard).</i>"
+        text = "This user currently isn't afk (not away from keyboard)."
     return text
-
 
 def __gdpr__(user_id):
     end_afk(user_id)
 
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=True)
-AFK_HANDLER = DisableAbleCommandHandler("cya", afk, run_async=True)
 AFK_REGEX_HANDLER = MessageHandler(Filters.regex("(?i)brb"), afk, friendly="afk", run_async=True)
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, no_longer_afk, run_async=True)
 AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, reply_afk, run_async=True)
